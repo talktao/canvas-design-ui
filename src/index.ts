e0ec4bfc635c5b43298986ac686f049ef8456b27
talktao <t1416249906@163.com>
2022-08-11 16:58:18 +0800

    feat：export ProgressCircle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export { default as WaterSphere } from './water-sphere/index';
+export { default as WaterSphere } from './component/water-sphere';
+export { default as ProgressCircle } from './component/progress-circle';
